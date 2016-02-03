c79d5fd033e362a45777690a20fc0e2bc2da16a0
Christopher Dwyer-Perkins <chrisdwyerperkins@gmail.com>
2016-02-03 15:20:21 -0400

    applied visuals to the app and also used a HTML file to populate the about info in a web view.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -23,6 +23,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider .setThumbImage(thumbImageNormal, forState: .Normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
+        
         startNewGame()
         updateLabels()
     }
@@ -63,7 +81,6 @@ class ViewController: UIViewController {
         var points = 100 - difference
         var title: String
         
-        print(difference)
         if difference == 0 {
             title = "Perfect!"
             points += 100
